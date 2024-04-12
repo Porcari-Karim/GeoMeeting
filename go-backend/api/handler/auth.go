bdf953214656087e02ffa8dead449eeeb9f86c08
Porcari-Karim <porcari.karim1@gmail.com>
2024-04-12 21:23:56 +0200

    Added Google OAuth endpoints

--- go-backend/api/handler/auth.go
+++ go-backend/api/handler/auth.go
@@ -1,8 +1,10 @@
 package handler
 
 import (
+	"fmt"
 	"html/template"
 	"net/http"
+	"os"
 )
 
 var authHandler *http.ServeMux
@@ -18,6 +20,9 @@ func initAuth(rootMux *http.ServeMux) {
 	authHandler.HandleFunc("GET /login", loginHandler)
 	authHandler.HandleFunc("POST /login", loginHandler)
 	authHandler.HandleFunc("GET /logout", logoutHandler)
+
+	authHandler.HandleFunc("/o/google/", googleOAuthHandler)
+	authHandler.HandleFunc("/o/google/callback/", googleOAuthCallbackHandler)
 }
 
 func loginHandler(w http.ResponseWriter, r *http.Request) {
@@ -32,4 +37,22 @@ func logoutHandler(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		panic(err)
 	}
+}
+
+func googleOAuthHandler(w http.ResponseWriter, r *http.Request) {
+	clientId := os.Getenv("GOOGLE_CLIENT_ID")
+	redirectURI := "http://" + r.Host + "/auth/o/google/callback/"
+	scope := "https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile"
+	authRedirectURL := fmt.Sprintf("https://accounts.google.com/o/oauth2/v2/auth?response_type=code&scope=%s&redirect_uri=%s&client_id=%s", scope, redirectURI, clientId)
+	http.Redirect(w, r, authRedirectURL, http.StatusTemporaryRedirect)
+}
+
+func googleOAuthCallbackHandler(w http.ResponseWriter, r *http.Request) {
+	userInfo, err := getGoogleUserInfoFromCallback(r)
+	if err != nil {
+		fmt.Println(err)
+		w.WriteHeader(http.StatusBadRequest)
+	}
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(userInfo.Email))
 }
---
+++ go-backend/api/handler/google.go
@@ -0,0 +1,84 @@
+package handler
+
+import (
+	"encoding/json"
+	"errors"
+	"io"
+	"net/http"
+	"net/url"
+	"os"
+)
+
+type GoogleTokenResponse struct {
+	AccessToken string `json:"access_token"`
+	ExpiresIn   int    `json:"expires_in"`
+	TokenType   string `json:"token_type"`
+	Scope       string `json:"scope"`
+}
+
+type GoogleUserBasicInfo struct {
+	Picture         string `json:"picture"`
+	IsEmailVerified bool   `json:"verified_email"`
+	Id              string `json:"id"`
+	Email           string `json:"email"`
+}
+
+func getGoogleUserInfoFromCallback(r *http.Request) (GoogleUserBasicInfo, error) {
+	tokenResponse, err := getGoogleAccessToken(r)
+	if err != nil {
+		return GoogleUserBasicInfo{}, err
+	}
+
+	bearer := "Bearer " + tokenResponse.AccessToken
+	req, _ := http.NewRequest("GET", "https://www.googleapis.com/userinfo/v2/me", nil)
+	req.Header.Add("Authorization", bearer)
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return GoogleUserBasicInfo{}, err
+	}
+	defer func(Body io.ReadCloser) {
+		err := Body.Close()
+		if err != nil {
+			panic(err)
+		}
+	}(resp.Body)
+
+	var userInfo GoogleUserBasicInfo
+	err = json.NewDecoder(resp.Body).Decode(&userInfo)
+	if err != nil {
+		return GoogleUserBasicInfo{}, err
+	}
+
+	return userInfo, nil
+
+}
+
+func getGoogleAccessToken(r *http.Request) (GoogleTokenResponse, error) {
+	queryParams := r.URL.Query()
+	data := url.Values{
+		"code":          {queryParams.Get("code")},
+		"client_id":     {os.Getenv("GOOGLE_CLIENT_ID")},
+		"client_secret": {os.Getenv("GOOGLE_CLIENT_SECRET")},
+		"redirect_uri":  {"http://" + r.Host + "/auth/o/google/callback/"},
+		"grant_type":    {"authorization_code"},
+	}
+
+	resp, err := http.PostForm("https://oauth2.googleapis.com/token", data)
+	if err != nil {
+		return GoogleTokenResponse{}, err
+	}
+	defer func(Body io.ReadCloser) {
+		err := Body.Close()
+		if err != nil {
+			panic(err)
+		}
+	}(resp.Body)
+
+	var res GoogleTokenResponse
+	err = json.NewDecoder(resp.Body).Decode(&res)
+	if err != nil {
+		return GoogleTokenResponse{}, errors.New("Error while decoding response from google: " + err.Error())
+	}
+	return res, nil
+}
